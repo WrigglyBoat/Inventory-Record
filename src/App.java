76b911aaba56c470ae095826c0ad1040c6505a34
Cory <corynthianjobs@gmail.com>
2024-02-28 09:16:14 -0500

    commit

--- src/App.java
+++ src/App.java
@@ -4,24 +4,25 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
-    public static String masterFile = "src\\MasterFile.txt";
-    public static String transactionFile = "src\\Transactions.txt";
-    public static String reportFile = "src\\ReportFile.txt";
+   public static String masterFile = "src\\MasterFile.txt";
+   public static String transactionFile = "src\\Transactions.txt";
+   public static String reportFile = "src\\ReportFile.txt";
+   public static String employees = "src\\Employees.txt";
 
-    public static void main(String[] args) throws Exception {
-    System.out.println("*The sections in order are Part ID, Part Name, Quantity on Hand, Order Point, and Site");
-    System.out.println("Stock Count");
-    System.out.println("------------");
+   public static void main(String[] args) throws Exception {
+   System.out.println("*The sections in order are Part ID, Part Name, Quantity on Hand, Order Point, and Site");
+   System.out.println("Stock Count");
+   System.out.println("------------");
 
-    File vfile = new File(masterFile);
-    Scanner computFile= new Scanner(vfile);  //compf = computer file
-    String [] master= new String [110];   
-    int a=0;
-    int b=5;
-    
-    for(int i=0; i<110; i++) {//Finds words 
-        master[i]=computFile.next();
-    }
+   File vfile = new File(masterFile);
+   Scanner computFile= new Scanner(vfile);  //compf = computer file
+   String [] master= new String [110];   
+   int a=0;
+   int b=5;
+   
+   for(int i=0; i<110; i++) {//Finds words 
+      master[i]=computFile.next();
+   }
 
     for(int j=0; j<22; j++) { //Vertical (How many iterations down)
         for(int k=a; k<b; k++) {  //Individual Horizontal Word
@@ -33,7 +34,7 @@ public class App {
     } 
     System.out.println();  
 
-    File vfile1 = new File("DS7 Transaction.txt");
+    File vfile1 = new File(transactionFile);
     Scanner compf1= new Scanner(vfile1);
     String [] transaction= new String [66];
     int a1=0;
@@ -44,7 +45,7 @@ public class App {
     }
     
     try {
-      FileWriter ttom = new FileWriter("DS7 Master File.txt");
+      FileWriter ttom = new FileWriter(reportFile);
       ttom.write(
                  "*"+master[0]+" "+master[1]+" 17 "+master[3]+" "+master[4]+"\n"+
                  master[5]+" "+master[6]+" 30 "+master[8]+" "+master[9]+"\n"+
@@ -69,13 +70,13 @@ public class App {
                  master[100]+" "+master[101]+" 5 "+master[103]+" "+master[104]+"\n"+
                  master[105]+" "+master[106]+" 0 (OOS) "+master[108]+" "+master[109]+"\n"); 
       ttom.close();
-      System.out.println("Master File......Updated");
+      System.out.println("Report File Made");
     }
     catch(IOException e) {
       e.printStackTrace();
     }
 
-    File vfile2= new File("DS7 Master File.txt");
+    File vfile2= new File(reportFile);
     Scanner compf2= new Scanner(vfile);  //compf = computer file
     String [] newmaster= new String [110];   //goes 20 spaces down
    
@@ -83,11 +84,11 @@ public class App {
          newmaster[i]=compf2.next();
     }
 
-    System.out.println();
     System.out.println();   
     //ID, Name, On Hand, Order point, Job Site
     System.out.println("* = Order More");
     System.out.println("OOS = Out of Stock");
+
     System.out.println("Atlanta Site");//Lines 0-4, 
     System.out.println("------------");
       
@@ -111,6 +112,7 @@ public class App {
       }
       System.out.println(); 
       System.out.println();
+
    System.out.println("Albany Site");
    System.out.println("-----------");
    
@@ -137,32 +139,27 @@ public class App {
    System.out.println("Decatur Site");
    System.out.println("------------");
    
-      for(int i=5; i<9; i++)
-      {
+      for(int i=5; i<9; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
       
-      for(int i=30; i<34; i++)
-      {
+      for(int i=30; i<34; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
       
-      for(int i=55; i<59; i++)
-      {
+      for(int i=55; i<59; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
       
-      for(int i=80; i<84; i++)
-      {
+      for(int i=80; i<84; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
       
-      for(int i=105; i<109; i++)
-      {
+      for(int i=105; i<109; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
@@ -170,26 +167,22 @@ public class App {
    System.out.println("Douglasville Site");
    System.out.println("-----------------");
    
-      for(int i=10; i<14; i++)
-      {
+      for(int i=10; i<14; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
       
-      for(int i=35; i<39; i++)
-      {
+      for(int i=35; i<39; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
       
-      for(int i=60; i<64; i++)
-      {
+      for(int i=60; i<64; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
       
-      for(int i=85; i<89; i++)
-      {
+      for(int i=85; i<89; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
@@ -197,26 +190,22 @@ public class App {
    System.out.println("Macon Site");
    System.out.println("----------");
    
-      for(int i=20; i<24; i++)
-      {
+      for(int i=20; i<24; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
       
-      for(int i=45; i<49; i++)
-      {
+      for(int i=45; i<49; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
       
-      for(int i=70; i<74; i++)
-      {
+      for(int i=70; i<74; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
       
-      for(int i=95; i<99; i++)
-      {
+      for(int i=95; i<99; i++) {
          System.out.print(newmaster[i]+" ");
       }
       System.out.println();
@@ -224,157 +213,134 @@ public class App {
       
    System.out.println("-Employees At/Over 40 Hours Across All Sites-");
    System.out.println("------------------------------------------");
-   File Pay = new File("DS Employees.txt");
+   File Pay = new File(employees);
    Scanner file = new Scanner(Pay);
    String [] hourly = new String [140];            
       int a2=0;
       int b2=5;
    //0-4 first iteration
-   for(int i=0; i<140; i++)
-{
+   for(int i=0; i<140; i++) {
       hourly[i]=file.next();
-}
-   for(int i=1; i<=2; i++)
-{
+   }
+
+   for(int i=1; i<=2; i++) {
       System.out.print(hourly[i]+" ");                  //Print file changes like this 
-} 
+   } 
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[4]);
    
-    for(int i=11; i<=12; i++)
-{
+   for(int i=11; i<=12; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[14]);
    
-   for(int i=21; i<=22; i++)
-{
+   for(int i=21; i<=22; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[24]);
    
-   for(int i=31; i<=32; i++)
-{
+   for(int i=31; i<=32; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[34]);
    
-   for(int i=41; i<=42; i++)
-{
+   for(int i=41; i<=42; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }     
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[44]);
    
-   for(int i=51; i<=52; i++)
-{
+   for(int i=51; i<=52; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[54]);
    
-   for(int i=56; i<=57; i++)
-{
+   for(int i=56; i<=57; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[59]);
    
-   for(int i=61; i<=62; i++)
-{
+   for(int i=61; i<=62; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[64]);
    
-   for(int i=66; i<=67; i++)
-{
+   for(int i=66; i<=67; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[69]);
    
-   for(int i=71; i<=72; i++)
-{
+   for(int i=71; i<=72; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[74]);
    
-   for(int i=76; i<=77; i++)
-{
+   for(int i=76; i<=77; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[79]);
    
-   for(int i=81; i<=82; i++)
-{
+   for(int i=81; i<=82; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[84]);
    
-   for(int i=86; i<=87; i++)
-{
+   for(int i=86; i<=87; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[89]);
    
-   for(int i=91; i<=92; i++)
-{
+   for(int i=91; i<=92; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[94]);
    
-   for(int i=96; i<=97; i++)
-{
+   for(int i=96; i<=97; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[99]);
    
-   for(int i=111; i<=112; i++)
-{
+   for(int i=111; i<=112; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }  
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[114]);
    
-   for(int i=116; i<=117; i++)
-{
+   for(int i=116; i<=117; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[119]);
    
-   for(int i=121; i<=122; i++)
-{
-      System.out.print(hourly[i]+" "); 
-}
+   for(int i=121; i<=122; i++) {
+      System.out.print(hourly[i]+" ");  
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[124]);
    
-   for(int i=131; i<=132; i++)
-{
+   for(int i=131; i<=132; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[134]);
    
-   for(int i=136; i<=137; i++)
-{
+   for(int i=136; i<=137; i++) {
       System.out.print(hourly[i]+" "); 
-}
+   }
    System.out.print("| ");
    System.out.println("Hours worked ---> "+hourly[139]);
-      
-   
-
-    }
+   }
 }
